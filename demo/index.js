fc4ddac6be415f14e71861ce310bbae4fff33896
leochocolat <l.mouraire@outlook.fr>
2025-08-13 15:25:04 +0200

    🐛 Fix demo

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,6 @@
 import scenes from './scenes';
 
 const urlParams = new URLSearchParams(location.search);
-const demoName = urlParams.get('demo');
+const demoName = urlParams.get('demo') || '';
 const scene = scenes[demoName.toLowerCase()] ? new scenes[demoName.toLowerCase()]() : new scenes.basic();
 scene.start();
