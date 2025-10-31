40bf6e511e95c135bee7889452cd85d214236d7b
Tom <thomas-dove@hotmail.co.uk>
2025-10-31 10:49:12 +0000

    Update rollup config to build as esm and ignore three

--- rollup.build.config.js
+++ rollup.build.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import glslify from 'rollup-plugin-glslify';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
@@ -9,15 +9,16 @@ export default {
     input: './src/index.js',
     output: {
         file: './build/bundle.js',
-        format: 'umd',
+        format: 'esm',
         name: 'bundle',
     },
+    external: ['three'],
     plugins: [
-        babel(),
+        peerDepsExternal(),
         resolve(),
+        babel(),
         commonjs(),
         terser(),
         glslify(),
-        peerDepsExternal(),
     ],
 };
